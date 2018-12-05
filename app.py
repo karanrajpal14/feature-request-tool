3ec67e933e77fe85fe1016ba0775f5990080af9e
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-12-05 21:08:27 +0530

    Reformatted code and added necessary comments

--- app.py
+++ app.py
@@ -6,19 +6,23 @@ app = Flask(__name__)
 conf = os.environ['APP_SETTINGS']
 app.config.from_object(conf)
 
+
 def log(message):
     if conf == "config.DevelopmentConfig":
         print(message)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/request_feature', methods=['POST'])
 def save_feature_request():
     feature_request = request.get_json()
     log(feature_request)
     return json.dumps(feature_request)
+
 
 if __name__ == '__main__':
     app.run()
