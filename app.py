31c97c6f49affe6a5d942eb4dc4badad62bfd1be
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-12-05 19:54:38 +0530

    Initial implementation of KnockoutJS form bindings

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
+import json
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+conf = os.environ['APP_SETTINGS']
+app.config.from_object(conf)
+
+def log(message):
+    if conf == "config.DevelopmentConfig":
+        print(message)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/request_feature', methods=['POST'])
+def save_feature_request():
+    feature_request = request.get_json()
+    log(feature_request)
+    return json.dumps(feature_request)
 
 if __name__ == '__main__':
     app.run()
