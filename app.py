cf12cb536b3f8e4b543f676a74e0078266b5cd37
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-12-05 19:10:19 +0530

    Configured templates with asset requirements

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('index.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
 
 if __name__ == '__main__':
     app.run()
