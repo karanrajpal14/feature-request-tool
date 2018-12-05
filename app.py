01731f114e686bf0278ff2d3634e2c94ba1fc3dd
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-12-05 19:38:10 +0530

    Initial mockup of UI

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
-def hello():
-    return render_template('index.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
