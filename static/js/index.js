5164070c0035d34e4b083b49fc542f5790d6fc08
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-12-06 21:02:26 +0530

    Added form validation features using KO JS

--- static/js/index.js
+++ static/js/index.js
@@ -17,12 +17,14 @@ let formModel = function() {
 	]);
 
 	// Get form imput data
-	self.title = ko.observable('');
-	self.description = ko.observable('');
-	self.selected_client = ko.observable(undefined);
-	self.selected_priority = ko.observable(undefined);
-	self.selected_area = ko.observable('');
-	self.deadline = ko.observable('');
+	self.title = ko.observable(undefined).extend({ required: true });
+	self.description = ko.observable(undefined).extend({ required: true });
+	self.selected_client = ko.observable(undefined).extend({ required: true });
+	self.selected_priority = ko.observable(undefined).extend({ required: true, number: true });
+	self.selected_area = ko.observable(undefined).extend({ required: true });
+	self.deadline = ko.observable(undefined).extend({ required: true });
+	self.request_successful = ko.observable(null);
+	self.request_failed = ko.observable(null);
 
 	self.selected_client.subscribe(newClient => {
 		self.fetch_priorities(newClient);
@@ -41,6 +43,56 @@ let formModel = function() {
 		}
 	};
 
+	self.request_successful_toggle = () => {
+		if (self.request_successful()) {
+			self.request_successful(null);
+		} else {
+			self.request_successful(true);
+			setTimeout(function () {
+				self.request_successful(null);
+			}, 4000);
+		}
+	}
+
+	self.request_failed_toggle = () => {
+		if (self.request_failed()) {
+			self.request_failed(null);
+		} else {
+			self.request_failed(true);
+			setTimeout(function () {
+				self.request_failed(null);
+			}, 4000);
+		}
+	}
+
+	self.request_validation_errors = ko.validation.group({
+		title: self.title,
+		description: self.description,
+		selected_client: self.selected_client,
+		selected_priority: self.selected_priority,
+		selected_area: self.selected_area,
+		deadline: self.deadline
+	})
+
+	self.validte_request = function () {
+		form_items = {
+			title: self.title.isValid(),
+			description: self.description.isValid(),
+			selected_client: self.selected_client.isValid(),
+			selected_priority: self.selected_priority.isValid(),
+			selected_area: self.selected_area.isValid(),
+			deadline: self.deadline.isValid()
+		}
+		for (item in form_items) {
+			if (form_items.hasOwnProperty(item)) {
+				if (!form_items[item]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
 	// amalgamate form inputs for POST body
 	self.post_payload = ko.pureComputed(() => {
 		return {
@@ -55,15 +107,38 @@ let formModel = function() {
 
 	// make feature request
 	self.save_form_input = () => {
-		$.ajax('/api/v1/feature', {
-			method: 'POST',
-			contentType: 'application/json',
-			data: ko.toJSON(self.post_payload()),
+		if (self.validte_request()) {
+			$.ajax('/api/v1/feature', {
+				method: 'POST',
+				contentType: 'application/json',
+				data: ko.toJSON(self.post_payload()),
 
-			success: (data, status) => {
-				self.refresh();
-			}
-		});
+				success: (data, status) => {
+					self.refresh();
+					self.title(null);
+					self.description(null);
+					self.selected_client(undefined);
+					self.selected_priority(undefined);
+					self.selected_area(undefined);
+					self.deadline(null);
+
+					self.title.isModified(false);
+					self.description.isModified(false);
+					self.selected_client.isModified(false);
+					self.selected_priority.isModified(false);
+					self.selected_area.isModified(false);
+					self.deadline.isModified(false);
+
+					self.request_successful_toggle(true);
+				},
+
+				error: () => {
+					self.request_failed_toggle(true);
+				}
+			});
+		} else {
+			self.request_validation_errors.showAllMessages(true);
+		}
 	};
 
 	self.get_all_feature_requests = ko.observableArray([]);
